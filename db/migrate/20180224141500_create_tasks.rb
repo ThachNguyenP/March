375b44438699141de069e5aed9f3870d9aa6cbd7
at-thachnguyen <xing.bairong1994@gmail.com>
2018-02-24 21:18:26 +0700

    create task module

---
+++ db/migrate/20180224141500_create_tasks.rb
@@ -0,0 +1,9 @@
+class CreateTasks < ActiveRecord::Migration[5.0]
+  def change
+    create_table :tasks do |t|
+      t.string :name
+      t.text :description
+      t.integer :order
+    end
+  end
+end
