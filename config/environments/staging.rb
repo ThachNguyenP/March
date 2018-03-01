f8c86d7cf2ae9b067cce109c4324e00441361304
at-thachnguyen <xing.bairong1994@gmail.com>
2018-03-01 17:51:04 +0700

    temp

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -70,6 +70,7 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.assets.precompile += %w( *.coffee ^[^_]*.scss *.scss.erb )
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
@@ -84,4 +85,3 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
-Rails.application.config.assets.precompile += %w( *.coffee ^[^_]*.scss *.scss.erb )
