2c904234eb4476724ed5a291aaf04ef14635664d
at-thachnguyen <xing.bairong1994@gmail.com>
2018-03-01 17:55:48 +0700

    temp

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  config.assets.precompile += %w( *.coffee ^[^_]*.scss *.scss.erb )
+  config.assets.precompile += %w( *.coffee ^[^_]*.scss *.scss.erb *.css *.js)
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
