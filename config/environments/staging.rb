a4a226659ea527cab4e925690a0747752a80f59e
at-thachnguyen <xing.bairong1994@gmail.com>
2018-03-02 09:10:28 +0700

    temp

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -16,11 +16,11 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = true
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
@@ -54,7 +54,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "March_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "Dingo_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
@@ -70,7 +70,6 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  config.assets.precompile += %w( *.coffee ^[^_]*.scss *.scss.erb *.css *.js)
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
